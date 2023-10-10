d0ebb72525eab1461b9f70a2a0aa740b820026f0
Imre Tabur <imre.tabur@mail.ee>
2023-10-10 09:35:50 +0300

    Version update script

--- git_version.py
+++ git_version.py
@@ -3,12 +3,18 @@ import subprocess
 
 from smi_python_tbi_runner.project import NAME, VERSION
 
-try:
-    git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8")
-    with open(os.path.join(os.path.dirname(__file__), NAME, 'project.py'), 'w') as file:
-        file.write(f'NAME = "{NAME}"\n')
-        file.write(f'VERSION = "{VERSION}"\n')
-        file.write(f'HASH = "{git_hash}"\n')
 
-except Exception as e:
-    print(f"Error: {e}")
+def main():
+    try:
+        git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8")
+        with open(os.path.join(os.path.dirname(__file__), NAME, 'project.py'), 'w') as file:
+            file.write(f'NAME = "{NAME}"\n')
+            file.write(f'VERSION = "{VERSION}"\n')
+            file.write(f'HASH = "{git_hash}"\n')
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
