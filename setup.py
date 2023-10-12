fe33e24cadfd254d40a87a97494d16f8ac8499c7
Imre Tabur <imre.tabur@mail.ee>
2023-10-12 13:32:15 +0300

    Name changes

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from smi_python_runner.project import NAME, VERSION
 setup(
     name=NAME,
     version=VERSION,
-    description='setmy.info Python TBI runner library.',
-    long_description='setmy.info Python TBI runner library.',
+    description='setmy.info Python runner library.',
+    long_description='setmy.info Python runner library.',
     author='Imre Tabur',
     author_email='info@setmy.info',
     license='MIT',
-    url='https://github.com/setmy-info/python-tbi-runner',
+    url='https://github.com/setmy-info/python-runner',
     packages=find_packages(),
     install_requires=[
         "smi-python-commons==0.3.3"
